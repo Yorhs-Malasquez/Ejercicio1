ddc8ffb088e905091db543c34f0b207d72fc0523
yorhs_malasquez <yorhs1688@gmail.com>
2022-10-22 21:30:18 -0500

    Proyecto inicial

---
+++ src/com/example/ejemplo1/ejemplo1.java
@@ -0,0 +1,23 @@
+package com.example.ejemplo1;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ejemplo1 {
+    public static void main(String[] args) {
+        List<String> companyList= Arrays.asList("Google","Yahoo","Facebook","","Twitter","LinkedIn");
+        System.out.println("Company List: "+companyList);
+
+        //find only empty list count
+        long emptyCount=companyList.stream().filter(cList->cList.isEmpty()).count();
+        System.out.println("Test1: # of Empty Strings: "+emptyCount);
+
+        //find company with character length more than 6
+        long lengthCount=companyList.stream().filter(x->x.length()>6).count();
+        System.out.println("Test2: of companies with char length>6: "+lengthCount);
+
+        //match the pattern wich starts with letter T
+        Long startCount=companyList.stream().filter(x->x.startsWith("T")).count();
+        System.out.println("Test 3: # of companies which name starts with letter T: "+startCount);
+    }
+}
