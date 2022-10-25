8bf4eb62b680cc8af23d9f4d43bf8c99b7ac3c60
yorhs_malasquez <yorhs1688@gmail.com>
2022-10-24 23:18:06 -0500

    Se agrego una linea de codigo inservible

--- src/com/example/ejemplo1/PersonaRegistro.java
+++ src/com/example/ejemplo1/PersonaRegistro.java
@@ -3,6 +3,7 @@ package com.example.ejemplo1;
 public record PersonaRegistro(String nombre, String apellido) {
     @Override
     public String toString() {
+        System.out.println("Esta instruccion es inservible");
         return "%s %s".formatted(nombre,apellido);
     }
 }
