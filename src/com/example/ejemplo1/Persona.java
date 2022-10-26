a824e8a8f53d6294d5fbc0d7278c5ca40447e9d1
yorhs_malasquez <yorhs1688@gmail.com>
2022-10-26 00:34:50 -0500

    Se reemplaza clase persona por personaregistro

--- src/com/example/ejemplo1/Persona.java
+++ src/com/example/ejemplo1/Persona.java
@@ -1,31 +1,10 @@
 package com.example.ejemplo1;
 
-public class Persona {
-    private String nombre, apellido;
-
-    public Persona(String nombre, String apellido) {
-        this.nombre = nombre;
-        this.apellido = apellido;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
+public record Persona(String nombre, String apellido) {
     @Override
     public String toString() {
-        return "%s %s".formatted(nombre, apellido);
+        //borre esto
+        //System.out.println("Esta instruccion es inservible");
+        return "%s %s".formatted(nombre,apellido);
     }
 }
